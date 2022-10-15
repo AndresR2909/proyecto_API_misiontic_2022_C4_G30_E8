198496faf4951373ef1c4c8e414a80618b9a234b
AndresR2909 <felipeandres29@hotmail.com>
2022-10-14 20:48:05 -0500

    First commit

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {ProyectoApiMisiontic2022C4G30E8Application} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new ProyectoApiMisiontic2022C4G30E8Application({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: ProyectoApiMisiontic2022C4G30E8Application;
+  client: Client;
+}
