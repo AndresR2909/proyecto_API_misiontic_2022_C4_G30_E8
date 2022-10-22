106c12cf366c5ec759602742e7bf3f33346024b4
AndresR2909 <felipeandres29@hotmail.com>
2022-10-21 19:40:18 -0500

    reparar merge index repositorio y controlador

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,7 +1,5 @@
-export * from './usuario.repository';
 export * from './aeropuerto.repository';
-<<<<<<< HEAD
-export * from './vuelo.repository';
-=======
->>>>>>> 1d637bc594f3fd5824d1344bae05bcb77173ced7
 export * from './ruta.repository';
+export * from './usuario.repository';
+export * from './vuelo.repository';
+
